b265bc1f2ef1282f25d261b9ecaf4c55700c9e9f
helpingstar <iamhelpingstar@gmail.com>
2021-12-30 02:31:40 +0900

    2021.12.30

--- Solved_Problem/BOJ_14499.py
+++ Solved_Problem/BOJ_14499.py
@@ -9,6 +9,8 @@ move_list = list(map(int, input().split()))
 
 dice = [0, 0, 0, 0, 0, 0, 0]
 
+# 정육면체는 가만히 있고 숫자와 판이 움직이는 개념
+
 def roll_dice(move):
     if move == 1:
         dice[1], dice[3], dice[6], dice[4] = dice[4], dice[1], dice[3], dice[6]
@@ -20,6 +22,8 @@ def roll_dice(move):
         dice[6], dice[5], dice[1], dice[2] = dice[5], dice[1], dice[2], dice[6]
 
 for move in move_list:
+    # 출력은 주사위 굴림과 x, y좌표의 관계에 대해 독립적이므로 순차적으로 진행해도 된다.
+    # 첫번째로 행렬에서의 포인터를 움직이는데 움직이지 못 할경우 continue
     if move == 1:
         if y == M-1:
             continue
@@ -39,6 +43,7 @@ for move in move_list:
     
     roll_dice(move)
     
+    # 기타 조건 수행
     if matrix[x][y] == 0:
         matrix[x][y] = dice[6]
     else:
