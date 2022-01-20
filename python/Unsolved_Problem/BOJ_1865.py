2bf6da1b71a3ea34541cc41b243d598dc95dac82
helpingstar <iamhelpingstar@gmail.com>
2022-01-20 09:28:47 +0900

    2022.01.20

--- python/Unsolved_Problem/BOJ_1865.py
+++ python/Unsolved_Problem/BOJ_1865.py
@@ -14,7 +14,8 @@ def bf(start):
             # 3 2 0
             # 2 3 -1
             # 3 2 -1
-            # 즉 시작점이 INF인 경우 문제를 풀지 못하게 된다.
+            # 즉 첫 "cur"이 INF인 경우 문제를 풀지 못하게 된다.
+            # ex) 첫 cur이 INF여서 지나쳤는데 첫 "cur"과 연계된 것들이 고립되었을때 문제를 풀지 못하게 된다.
             # 벨만포드 알고리즘은 어느곳으로 시작해도 사이클 판단이 가능하다.
             if distance[next_node] > distance[cur] + cost:
                 distance[next_node] = distance[cur] + cost
@@ -37,4 +38,40 @@ for _ in range(tc):
         print('YES')
     else:
         print('NO')
-    
+        
+        
+import sys
+
+def solution(N, edges):
+    g = [100000000 for _ in range(N + 1)]
+    g[0] = 0
+    for _ in range(N):
+        update = False
+        for s, e, t in edges:
+            if g[e] > g[s] + t:
+                g[e] = g[s] + t
+                update = True
+        
+        if not update: break
+
+    for s, e, t in edges:
+        if g[e] > g[s] + t:
+            return 'YES'
+
+    return 'NO'
+
+if __name__ == '__main__':
+    TC = int(sys.stdin.readline())
+    for _ in range(TC):
+        N, M, W = map(int, sys.stdin.readline().split())
+        edges = []
+        for _ in range(M):
+            s, e, w = map(int, sys.stdin.readline().split())
+            edges.append((s, e, w))
+            edges.append((e, s, w))
+        
+        for _ in range(W):
+            s, e, w = map(int, sys.stdin.readline().split())
+            edges.append((s, e, -w))
+        
+        sys.stdout.write('%s\n'%solution(N, edges))
