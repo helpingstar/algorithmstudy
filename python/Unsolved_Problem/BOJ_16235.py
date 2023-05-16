779e1832349f630bde1a32d7cafc07c824424551
helpingstar <iamhelpingstar@gmail.com>
2023-05-16 19:45:38 +0900

    solve 1 unsolve 1

--- python/Unsolved_Problem/BOJ_16235.py
+++ python/Unsolved_Problem/BOJ_16235.py
@@ -1,53 +1,50 @@
 import sys
-import heapq
-
 input = sys.stdin.readline
+
+dr = (-1, -1, -1,  0, 0,  1, 1, 1)
+dc = (-1,  0,  1, -1, 1, -1, 0, 1)
 
 N, M, K = map(int, input().split())
 
-dx = (-1, -1, -1, 0, 0, 1, 1, 1)
-dy = (-1, 0, 1, -1, 1, -1, 0, 1)
-
-nutritions = [list(map(int, input().split())) for _ in range(N)]
+A = [list(map(int, input().split())) for _ in range(N)]
 board = [[5] * N for _ in range(N)]
-trees = [[[] for _ in range(N)] for _ in range(N)]
 
+trees = []
 for _ in range(M):
-    n = int
     x, y, z = map(int, input().split())
-    heapq.heappush(trees[x-1][y-1], z)
+    x -= 1
+    y -= 1
+    trees.append((z, x, y))
 
-for _ in range(K):
-    child = []
-    for r in range(N):
-        for c in range(N):
-            if trees[r][c]:
-                new_trees = []
-                dead = 0
-                while trees[r][c]:
-                    age = heapq.heappop(trees[r][c])
-                    if board[r][c] >= age:
-                        board[r][c] -= age
-                        age += 1
-                        if age % 5 == 0:
-                            child.append((r, c))
-                        heapq.heappush(new_trees, age)
-                    else:
-                        dead += age // 2
-                trees[r][c] = new_trees
-                board[r][c] += dead
-            board[r][c] += nutritions[r][c]
-    for r, c in child:
+for i in range(K):
+    new_trees = []
+    dead_trees = []
+    spread_trees = []
+    # spring
+    for age, r, c in sorted(trees):
+        if age <= board[r][c]:
+            board[r][c] -= age
+            new_trees.append((age+1, r, c))
+            if (age+1) % 5 == 0:
+                spread_trees.append((r, c))
+        else:
+            dead_trees.append((age, r, c))
+    # summer
+    for age, r, c in dead_trees:
+        board[r][c] += age // 2
+    # fall
+    for r, c in spread_trees:
         for i in range(8):
-            nr = r + dx[i]
-            nc = c + dy[i]
-
+            nr = r + dr[i]
+            nc = c + dc[i]
             if not (0 <= nr < N and 0 <= nc < N):
                 continue
-            heapq.heappush(trees[nr][nc], 1)
-
-ans = 0
-for r in range(N):
-    for c in range(N):
-        ans += len(trees[r][c])
-print(ans)
+            new_trees.append((1, nr, nc))
+    # winter
+    for r in range(N):
+        for c in range(N):
+            board[r][c] += A[r][c]
+    
+    trees = new_trees
+    
+print(len(trees))
--- python/Unsolved_Problem/BOJ_17143.py
+++ python/Unsolved_Problem/BOJ_17143.py
@@ -2,13 +2,16 @@ import sys
 
 input = sys.stdin.readline
 
-n_row, n_col, n_shark = map(int, input().split())
-shark_set = set()
-
-
-
-for _ in range(n_shark):
+R, C, M = map(int, input().split())
+board = [[[] for _ in range()]]
+# s : 속력
+# d : 방향
+#   1 : 위
+#   2 : 아래
+#   3 : 오른쪽
+#   4 : 왼쪽
+for _ in range(M):
     r, c, s, d, z = map(int, input().split())
-    shark_set.add((r, c, s, d, z))
-
-def next_shark_pos(r, c, s, d):
+    r -= 1
+    c -= 1
+    board[r][c]
