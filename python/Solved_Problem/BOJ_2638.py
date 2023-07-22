7fd49199c29a01cbb09dc1859299089bcd42538d
helpingstar <iamhelpingstar@gmail.com>
2023-07-23 08:31:26 +0900

    resolve

--- python/Solved_Problem/BOJ_2638.py
+++ python/Solved_Problem/BOJ_2638.py
@@ -1,56 +1,54 @@
-import sys
 from collections import deque
-from collections import defaultdict
+import sys
+
 input = sys.stdin.readline
 
-n, m = map(int, input().split())
 
-matrix = []
+def cheese(board):
+    for r in range(len(board)):
+        for c in range(len(board[0])):
+            if board[r][c] == 1:
+                return True
+    return False
 
-for _ in range(n):
-    matrix.append(list(map(int, input().split())))
 
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
+def solution():
+    R, C = map(int, input().split())
+    board = [list(map(int, input().split())) for _ in range(R)]
+    dr = (-1, 1, 0, 0)
+    dc = (0, 0, -1, 1)
 
-def bfs():
-    entered = []
-    for _ in range(n):
-        entered.append([False] * m)
-    
-    cheese_touch = defaultdict(int)
-    all_zero = True
-    queue = deque()
-    queue.append((0, 0))
-    while queue:
-        x, y = queue.popleft()
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if not (0 <= nx < n and 0 <= ny < m):
-                continue
-            if entered[nx][ny]:
-                continue
-            if matrix[nx][ny] == 1:
-                cheese_touch[(nx, ny)] += 1
-                all_zero = False
-            else:
-                entered[nx][ny] = True
-                queue.append((nx, ny))
-    for key in cheese_touch:
-        cx, cy = key
-        if cheese_touch[(cx,cy)] >= 2:
-            matrix[cx][cy] = 0
-    return all_zero
+    cnt = 0
+    while True:
+        if not cheese(board):
+            return cnt
+        cnt += 1
+        visited = set()
+        q = deque()
+        visited.add((0, 0))
+        q.append((0, 0))
+        touch = dict()
+        while q:
+            r, c = q.popleft()
+            for i in range(4):
+                nr = r + dr[i]
+                nc = c + dc[i]
 
-total = 0
-for line in matrix:
-    total += sum(line)
-if total == 0:
-    print(0)
-else:
-    count = 0
-    while not bfs():
-        count += 1
+                if not (0 <= nr < R and 0 <= nc < C):
+                    continue
+                if (nr, nc) in visited:
+                    continue
+                if board[nr][nc] == 0:
+                    q.append((nr, nc))
+                    visited.add((nr, nc))
+                else:
+                    if (nr, nc) not in touch:
+                        touch[(nr, nc)] = 1
+                    else:
+                        touch[(nr, nc)] += 1
+        for (r, c), t in touch.items():
+            if t >= 2:
+                board[r][c] = 0
 
-    print(count)
+
+print(solution())
