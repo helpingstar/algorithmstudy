033a9c90734314269b486a56c9a2a6ca4ea66e94
helpingstar <iamhelpingstar@gmail.com>
2023-07-26 16:44:22 +0900

    resolve 1

--- python/Solved_Problem/BOJ_17471.py
+++ python/Solved_Problem/BOJ_17471.py
@@ -1,50 +1,65 @@
 import sys
-from collections import deque
 from itertools import combinations
+from collections import deque
 input = sys.stdin.readline
 
-n_city = int(input())
 
-populations = [0] + list(map(int, input().split()))
+def solution():
+    N = int(input())
+    populations = [0] + list(map(int, input().split()))
+    t_populations = sum(populations)
+    graph = [[] for _ in range(N+1)]
+    for i in range(1, N+1):
+        graph[i] = list(map(int, input().split()))[1:]
+    all_combinations = []
+    for i in range(1,  N // 2 + 1):
+        for comb in combinations(range(1, N+1), i):
+            team1_score = 0
+            for i in comb:
+                team1_score += populations[i]
+            team2_score = t_populations - team1_score
 
-graph = [[]]
+            all_combinations.append((abs(team1_score-team2_score), comb))
+    all_combinations.sort()
 
-for i in range(1, n_city+1):
-    temp = list(map(int, input().split()))
-    graph.append(temp[1:])
+    for diff, comb in all_combinations:
+        cnt = 1
+        start1 = comb[0]
+        for i in range(1, N+1):
+            if i not in comb:
+                start2 = i
+                break
+        visited = [False] * (N+1)
+        visited[start1] = True
+        q = deque([start1])
+        while q:
+            now = q.popleft()
+            for next_ in graph[now]:
+                if next_ not in comb:
+                    continue
+                if visited[next_]:
+                    continue
+                q.append(next_)
+                visited[next_] = True
+                cnt += 1
+        if cnt != len(comb):
+            continue
+        visited[start2] = True
+        q = deque([start2])
+        cnt += 1
+        while q:
+            now = q.popleft()
+            for next_ in graph[now]:
+                if next_ in comb:
+                    continue
+                if visited[next_]:
+                    continue
+                q.append(next_)
+                visited[next_] = True
+                cnt += 1
+        if cnt == N:
+            return diff
+    return -1
 
-def bfs(arr):
-    # print(f'[debug]  {arr}')
-    start = arr[0]
-    score = populations[start]
-    visited = set()
-    visited.add(start)
-    q = deque()
-    q.append(start)
-    while q:
-        now = q.popleft()
-        for next in graph[now]:
-            if next in visited:
-                continue
-            if next not in arr:
-                continue
-            q.append(next)
-            visited.add(next)
-            score += populations[next]
-    return score, len(visited)
 
-ans = 100000
-for i in range(1, n_city // 2 + 1):
-    for comb in combinations(range(1, n_city+1), i):
-        # print(f'debug [comb] : {comb}')
-        s1, v1 = bfs(comb)
-        s2, v2 = bfs([i for i in range(1, n_city+1) if i not in comb])
-        if v1+v2 == n_city:
-            # print(f'debug s1:{s1}, v1:{v1}')
-            # print(f'debug s2:{s2}, v2:{v2}')
-            ans = min(ans, abs(s1-s2))
-
-if ans == 100000:
-    print(-1)
-else:
-    print(ans)
+print(solution())
