5d428d1f57c14cb89929a04f9518fb0ad78b4db0
helpingstar <iamhelpingstar@gmail.com>
2023-08-15 05:27:02 +0900

    resolve 1

--- python/Solved_Problem/BOJ_1446.py
+++ python/Solved_Problem/BOJ_1446.py
@@ -1,44 +1,24 @@
 import sys
-from collections import defaultdict
-import heapq
 
 input = sys.stdin.readline
-INF = 100000
 
-distance = [INF] * 10001
-n_road, target = map(int, input().split())
-graph = defaultdict(list)
-jirm_set = set()
-for _ in range(n_road):
-    a, b, c = map(int, input().split())
-    if b > target:
-        continue
-    graph[a].append((b, c))
-    jirm_set.add(a)
 
-jirm_set.add(target)
+def solution():
+    N, D = map(int, input().split())
+    fast = []
+    for _ in range(N):
+        s, e, dist = map(int, input().split())
+        if e <= D:
+            fast.append((s, e, dist))
+    dp = [i for i in range(D+1)]
+    fast.sort(key=lambda x: x[1])
 
-# print(graph)
-# print(f'[debug]  jirm_set: {jirm_set}')
-def dijkstra():
-    q = []
-    heapq.heappush(q, (0, 0))
-    while q:
-        dist, now = heapq.heappop(q)
-        # print(f'[debug] now: {now}, dist: {dist}')
-        if now == target:
-            return dist
-        if distance[now] < dist:
-            continue
-        for next, new_dist in graph[now]:
-            cost = dist + new_dist
-            if cost < distance[next]:
-                heapq.heappush(q, (cost, next))
+    for s, e, dist in fast:
+        if dp[e] > dp[s] + dist:
+            dp[e] = dp[s] + dist
+            for i in range(e+1, D+1):
+                dp[i] = min(dp[i], dp[i-1] + 1)
+    print(dp[D])
 
-        for jirm in jirm_set:
-            if jirm <= now:
-                continue
-            if dist + (jirm - now) < distance[jirm]:
-                heapq.heappush(q, (dist+jirm-now, jirm))
 
-print(dijkstra())
+solution()
