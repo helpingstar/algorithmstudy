deb1bad616e23fa98dce77c7e6d9ec8bfb585fb7
helpingstar <iamhelpingstar@gmail.com>
2023-08-22 08:33:31 +0900

    resolve 1

--- python/Solved_Problem/BOJ_1446.py
+++ python/Solved_Problem/BOJ_1446.py
@@ -1,24 +1,42 @@
 import sys
-
+import heapq
 input = sys.stdin.readline
 
 
 def solution():
-    N, D = map(int, input().split())
-    fast = []
-    for _ in range(N):
-        s, e, dist = map(int, input().split())
-        if e <= D:
-            fast.append((s, e, dist))
-    dp = [i for i in range(D+1)]
-    fast.sort(key=lambda x: x[1])
+    dr = (-1, 1, 0, 0)
+    dc = (0, 0, -1, 1)
+    INF = int(1e9)
+    ITER = 0
+    while True:
+        ITER += 1
+        SIZE = int(input())
+        if SIZE == 0:
+            return
+        board = [list(map(int, input().split())) for _ in range(SIZE)]
+        dist = [[INF] * SIZE for _ in range(SIZE)]
 
-    for s, e, dist in fast:
-        if dp[e] > dp[s] + dist:
-            dp[e] = dp[s] + dist
-            for i in range(e+1, D+1):
-                dp[i] = min(dp[i], dp[i-1] + 1)
-    print(dp[D])
+        q = []
+        heapq.heappush(q, (board[0][0], 0, 0))
+        dist[0][0] = board[0][0]
+
+        while q:
+            cnt, r, c = heapq.heappop(q)
+
+            if r == SIZE-1 and c == SIZE-1:
+                break
+
+            for i in range(4):
+                nr = r + dr[i]
+                nc = c + dc[i]
+
+                if not (0 <= nr < SIZE and 0 <= nc < SIZE):
+                    continue
+                if dist[r][c] + board[nr][nc] >= dist[nr][nc]:
+                    continue
+                heapq.heappush(q, (cnt + board[nr][nc], nr, nc))
+                dist[nr][nc] = dist[r][c] + board[nr][nc]
+        print(f"Problem {ITER}: {dist[SIZE-1][SIZE-1]}")
 
 
 solution()
