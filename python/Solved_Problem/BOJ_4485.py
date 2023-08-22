4fb4abd1e684b6f7ad70738b25ab607c77702443
helpingstar <iamhelpingstar@gmail.com>
2023-08-23 08:38:06 +0900

    resolve BOJ_4485 / 96 -> 92

--- python/Solved_Problem/BOJ_4485.py
+++ python/Solved_Problem/BOJ_4485.py
@@ -1,43 +1,42 @@
 import sys
 import heapq
 input = sys.stdin.readline
-INF = int(1e6)
-
-dx = (-1, 1, 0, 0)
-dy = (0, 0, -1, 1)
 
 
-def solution(n):
-    board = [list(map(int, input().split())) for _ in range(n)]
-    distance = [[INF] * n for _ in range(n)]
-    distance[0][0] = board[0][0]
-    q = []
-    heapq.heappush(q, (board[0][0], 0, 0))
-    while q:
-        step, x, y = heapq.heappop(q)
-        if distance[x][y] < step:
-            continue
+def solution():
+    dr = (-1, 1, 0, 0)
+    dc = (0, 0, -1, 1)
+    INF = int(1e9)
+    ITER = 0
+    while True:
+        ITER += 1
+        SIZE = int(input())
+        if SIZE == 0:
+            return
+        board = [list(map(int, input().split())) for _ in range(SIZE)]
+        dist = [[INF] * SIZE for _ in range(SIZE)]
 
-        # print(step, x, y)
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
+        q = []
+        heapq.heappush(q, (board[0][0], 0, 0))
+        dist[0][0] = board[0][0]
 
-            if not (0 <= nx < n and 0 <= ny < n):
-                continue
-            if nx == n-1 and ny == n-1:
-                return step + board[n-1][n-1]
+        while q:
+            cnt, r, c = heapq.heappop(q)
 
-            cost = step + board[nx][ny]
-            if cost < distance[nx][ny]:
-                distance[nx][ny] = cost
-                heapq.heappush(q, (cost, nx, ny))
+            if r == SIZE-1 and c == SIZE-1:
+                break
+
+            for i in range(4):
+                nr = r + dr[i]
+                nc = c + dc[i]
+
+                if not (0 <= nr < SIZE and 0 <= nc < SIZE):
+                    continue
+                if dist[r][c] + board[nr][nc] >= dist[nr][nc]:
+                    continue
+                heapq.heappush(q, (cnt + board[nr][nc], nr, nc))
+                dist[nr][nc] = dist[r][c] + board[nr][nc]
+        print(f"Problem {ITER}: {dist[SIZE-1][SIZE-1]}")
 
 
-cnt = 1
-while True:
-    n = int(input())
-    if n == 0:
-        break
-    print(f'Problem {cnt}: {solution(n)}')
-    cnt += 1
+solution()
