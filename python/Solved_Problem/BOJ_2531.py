4ac033268fc8436ce3e766539522e0cd8722f135
helpingstar <iamhelpingstar@gmail.com>
2023-09-14 15:34:12 +0900

    BOJ_2531 80 -> 64

--- python/Solved_Problem/BOJ_2531.py
+++ python/Solved_Problem/BOJ_2531.py
@@ -1,44 +1,46 @@
 import sys
 
-input = sys.stdin.readline
 
-n_dish, n_sushi, n_continue, coupon = map(int, input().split())
+def solution():
+    input = sys.stdin.readline
 
-belt = [int(input()) for _ in range(n_dish)]
+    n_dish, kind_sushi, combo_dish, coupon = map(int, input().split())
+    sushis = [int(input()) for _ in range(n_dish)]
 
-eat = [0] * (n_sushi + 1)
-count = 0
-ans = 0
-for i in range(n_continue):
-    if not eat[belt[i]]:
-        count += 1
-    eat[belt[i]] += 1
+    answer = 0
 
-ans = count
-if not eat[coupon]:
-    ans += 1
+    eat_table = [0] * (kind_sushi + 1)
+    eat_count = 0
+    for i in range(combo_dish - 1):
+        if eat_table[sushis[i]] == 0:
+            eat_count += 1
+        eat_table[sushis[i]] += 1
 
-tail = 0
-head = n_continue - 1
-# print(f'[debug]  eat: {eat}')
-for i in range(n_dish):
-    head += 1
-    new_head = head % n_dish
+    for i in range(n_dish):
+        if eat_table[sushis[(i + combo_dish - 1) % n_dish]] == 0:
+            eat_count += 1
+        eat_table[sushis[(i + combo_dish - 1) % n_dish]] += 1
+        # print("debug", (i + combo_dish) % n_dish, eat_count)
+        # print(eat_table)
+        answer = max(answer, eat_count + 1 if eat_table[coupon] == 0 else eat_count)
 
-    if not eat[belt[new_head]]:
-        count += 1
-    eat[belt[new_head]] += 1
-    new_tail = tail % n_dish
-    if eat[belt[new_tail]] == 1:
-        count -= 1
-    eat[belt[new_tail]] -= 1
-    tail += 1
+        if eat_table[sushis[i]] == 1:
+            eat_count -= 1
+        eat_table[sushis[i]] -= 1
 
-    if eat[coupon]:
-        bonus = 0
-    else:
-        bonus = 1
-    # print(f'[debug]  eat: {eat}')
-    ans = max(ans, count + bonus)
+    print(answer)
 
-print(ans)
+
+solution()
+
+"""
+8 30 4 30
+9
+25
+7
+9
+7
+30
+2
+7
+"""
